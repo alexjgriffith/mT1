6441846aa6de8468430e58ae4999c19a9d4df4a3
alexjgriffith <griffitaj@gmail.com>
2016-08-24 17:23:39 -0400

    initial commit

---
+++ R/data.r
@@ -0,0 +1,26 @@
+#' Fasta
+#' 
+#' The nucleotide values for the genomic locations defined by peaks
+#' @format A DANStringSet with 5670 members:
+"fasta"
+
+#' Jaspar
+#'
+"jaspar"
+
+#' objMT1
+#'
+"objMT1"
+
+#' peaks
+#'
+#' A set of TAL1 peaks combined from 22 Hematopoietic and Endothelial
+#' datasets
+#'
+#' @format A data frame with 5670 rows and 3 variables:
+#' \describe{
+#'   \item{chr}{chromosome}
+#'   \item{start}{starting base pair location}
+#'   \item{end}{ending base pair location}
+#' }
+"peaks"
---
+++ R/mT1.r
@@ -0,0 +1,6 @@
+#' Motifs in Tandem with One Another
+#'
+#' @docType package
+#' @name mT1
+#' @author "Alexander Griffith <griffitaj@@gmail.com>"
+NULL
